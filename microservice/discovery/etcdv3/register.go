692a91d2c52ae51f7a3f797cc5cd31622a837c6d
pandaychen <pandaychen@tencent.com>
2021-03-08 18:39:36 +0800

    fix

--- microservice/discovery/etcdv3/register.go
+++ microservice/discovery/etcdv3/register.go
@@ -3,9 +3,11 @@ package etcdv3
 //etcdv3的服务注册封装
 
 import (
+	"encoding/json"
 	"time"
 
 	com "github.com/pandaychen/grpc-wrapper-framework/microservice/discovery/common"
+	etcd3 "go.etcd.io/etcd/clientv3"
 	"go.uber.org/zap"
 	"golang.org/x/net/context"
 )
@@ -16,43 +18,45 @@ const (
 )
 
 type EtcdRegister struct {
-	//Etcd3Client *etcdv3.Client
-	Logger *zap.Logger
-	Key    string //service uniq-key
-	Value  string //micro-service ip+port+weight
-	Ttl    time.Duration
-	Ctx    context.Context
-	Cancel context.CancelFunc
+	Etcd3Client *etcd3.Client
+	Logger      *zap.Logger
+	Key         string //service uniq-key
+	Value       string //micro-service ip+port+weight
+	Ttl         time.Duration
+	Ctx         context.Context
+	Cancel      context.CancelFunc
 }
 
-func NewRegister(config *com.RegisterConfig) (EtcdRegister, error) {
-	/*
-		client, err := etcdv3.New(config.EtcdConfig)
-		if err != nil {
-			config.Logger.Error("Create etcdv3 client error", zap.String("errmsg", err.Error()))
-			return nil, err
-		}
+func NewRegister(config *com.RegisterConfig) (*EtcdRegister, error) {
+	//TODO：use https://github.com/pandaychen/etcd_tools/blob/master/clientv3.go instead
+	etcdConfg := etcd3.Config{
+		Endpoints: config.Endpoint,
+	}
 
-		//check format
-		val, err := json.Marshal(config.NodeData)
-		if err != nil {
-			config.Logger.Error("Create etcdv3 value error", zap.String("errmsg", err.Error()))
-			return nil, err
-		}
+	client, err := etcdv3.New(etcdConfg)
+	if err != nil {
+		config.Logger.Error("[NewRegister]Create etcdv3 client error", zap.String("errmsg", err.Error()))
+		return nil, err
+	}
 
-		ctx, cancel := context.WithCancel(context.Background())
-		registry := &EtcdRegister{
-			Etcd3Client: client,
-			Logger:      config.Logger,
-			Ttl:         config.Ttl / time.Second,
-			Ctx:         ctx,
-			Cancel:      cancel,
-			Key:         BuildEtcdKey(config),
-			Value:       string(val),
-		}
-		return registry, nil
-	*/
-	return nil, nil
+	//check format
+	val, err := json.Marshal(config.NodeData)
+	if err != nil {
+		config.Logger.Error("[NewRegister]Create etcdv3 value error", zap.String("errmsg", err.Error()))
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	registry := &EtcdRegister{
+		Etcd3Client: client,
+		Logger:      config.Logger,
+		Ttl:         config.Ttl / time.Second,
+		Ctx:         ctx,
+		Cancel:      cancel,
+		Key:         config.BuildEtcdKey(),
+		Value:       string(val),
+	}
+	return registry, nil
 }
 
 func (r *EtcdRegister) ServiceRegister() error {
@@ -68,9 +72,7 @@ func (r *EtcdRegister) Close() {
 }
 
 /*
-func BuildEtcdKey(config RegisterConfig) string {
-	return fmt.Sprintf("/%s/%s/%s/%s", config.RootName, config.ServiceName, config.ServiceVersion, config.ServiceNodeID)
-}
+
 
 //call etcd api
 func (et *EtcdRegister) RegisterByApi() error {
